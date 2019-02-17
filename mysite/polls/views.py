280f030be97bb8b3be07f76f1bd6797a42357e0e
ldgeao99 <ldgeao99@gmail.com>
2019-02-17 22:05:07 +0900

    Question 모델 import index메소드 수정

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, CodeSquad")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]   # order_by('속성명') : 오름차순, ('-속성명') : 내림차순
+    output = " ,".join([q.question_text for q in latest_question_list]) # 리스트를 문자열로 바꿔주고 " ,"로 원소 중간중간 넣어주겠다는 것.
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
