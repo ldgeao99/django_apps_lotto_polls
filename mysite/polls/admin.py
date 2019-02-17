794276144f16fee5fcec3fe04c758113a865802a
ldgeao99 <ldgeao99@gmail.com>
2019-02-17 21:15:16 +0900

    admin페이지에서 보여지도록 모델 추가

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# 아래의 코드를 입력해줘야 admin 사이트에 보여진다.
+from .models import Question, Choice
+admin.site.register(Question)
+admin.site.register(Choice)
