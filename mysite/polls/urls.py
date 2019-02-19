8d647b1e67809297dea3b58bfd61e2e496373b7d
ldgeao99 <ldgeao99@gmail.com>
2019-02-20 01:55:10 +0900

    function views 방식으로 구현된 라우팅을 Class-based views 방식으로 교체. vote관련된것은 제외.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,8 +7,13 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name = "detail"),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name = "results"),
+    #리스트뷰는 똑같은 것만 보여주는 페이지 이기 때문에 파라미터를 받지 않는다.
+    url(r'^$', views.IndexView.as_view(), name = 'index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name = "detail"),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name = "results"),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name = "vote"),
+
+    #url(r'^$', views.index, name = 'index'),
+    #url(r'^(?P<question_id>[0-9]+)/$', views.detail, name = "detail"),
+    # url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name = "results"),
 ]
