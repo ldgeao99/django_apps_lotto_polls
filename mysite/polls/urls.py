66083bfc9668b4263536befb60d3a1a2bc27dedb
ldgeao99 <ldgeao99@gmail.com>
2019-02-20 22:00:10 +0900

    main url추가

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,6 +7,7 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
+    url(r'^main/$', views.main, name='main'),
     url(r'^$', views.IndexView.as_view(), name = 'index'), #ListView는 내부에서 지정한 객체들을 가져오므로 파라미터를 받지 않는다.
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name = "detail"),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name = "results"),
