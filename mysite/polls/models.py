be2d9f415ebb6c6692dcfa0b3a45a4023e5ef49c
ldgeao99 <ldgeao99@gmail.com>
2019-02-20 17:44:28 +0900

    was_published_recently에 대한 값이 pub_date 순으로 정렬하는 버튼이 활성화 되도록 하였고, True, False값 대신에 버튼 모양이 O,X로 보이게하였다. 또한 보여지는 속성 이름을 대체한 코드

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -16,6 +16,10 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    was_published_recently.admin_order_field = 'pub_date'            # 정렬버튼을 활성화 하고, 누를시 pub_date기준으로 정렬이 수행됨.
+    was_published_recently.boolean = True                            # 값이 O, X 모양으로 나타나게 됨.
+    was_published_recently.short_description = 'published recently?' # WAS PUBLISHED RECENTLY 대신 여기에 입력해준 값이 보여짐.
+
 class Choice(models.Model):
     #  Foeign Key 설정, on_delete 설정 : 참조한 키가 삭제되면 그 키와 관련된 데이터를 같이 삭제해주세요
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
