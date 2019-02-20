2d56ea2b1f689166acaf0befe2a4cd48d73a14ba
ldgeao99 <ldgeao99@gmail.com>
2019-02-20 15:53:03 +0900

    추가적인 view 테스트를 위한 QuestionViewTests2클래스 및 메소드 추가

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -10,12 +10,73 @@ def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date = time)
 
-class QuestionViewTests(TestCase):
+class QuestionViewTests1(TestCase):
     def test_index_view_with_future_question(self):
         create_question(question_text='Future question', days=30)
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)                            # 상태코드가 200이야?
         self.assertQuerysetEqual(response.context['latest_question_list'], []) # 쿼리셋 결과가 []이야?
+
+class QuestionViewTests2(TestCase):
+    # 아무 객체도 없는경우 아무것도 안보여줌을 테스트.
+    def test_index_view_with_no_questions(self):
+        """
+        If no questions exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")    # 아무것도 없는경우 "No polls..."가 찍여야 함(index.html참고)
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    # 과거의 날짜를 가진 객체는 보여짐을 테스트.
+    def test_index_view_with_a_past_question(self):
+        """
+        Questions with a pub_date in the past should be displayed on the
+        index page.
+        """
+        create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    # 미래의 날짜를 가진 객체는 보이지 않아야 함을 테스트.
+    def test_index_view_with_a_future_question(self):
+        """
+        Questions with a pub_date in the future should not be displayed on
+        the index page.
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    # 과거의 날자와 미래의 날짜를 가진 객체가 모두 존재한다면 과거의 날짜를 가진 객체만 보여야함을 테스트.
+    def test_index_view_with_future_question_and_past_question(self):
+        """
+        Even if both past and future questions exist, only past questions
+        should be displayed.
+        """
+        create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+    # 2개의 과거 날짜를 가진 객체가 있을 때 둘 다 보여지며, 시간순서대로 잘 정렬되어 나오는지 테스트.
+    def test_index_view_with_two_past_questions(self):
+        """
+        The questions index page may display multiple questions.
+        """
+        create_question(question_text="Past question 1.", days=-30)
+        create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question 2.>', '<Question: Past question 1.>']
+        )
 
 class QuestionMethodsTests(TestCase):
     # 7일 미래의 시간을 테스트하는 코드
