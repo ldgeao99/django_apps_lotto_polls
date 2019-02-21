6837818edc238d7b9cc39a7d5b1661d02cce44cf
ldgeao99 <ldgeao99@gmail.com>
2019-02-21 19:59:45 +0900

    index메소드를 지워주고 index2가 index가 되도록 수정

--- mysite/lotto/views.py
+++ mysite/lotto/views.py
@@ -5,10 +5,7 @@ from django.http import HttpResponse
 from .models import GuessNumbers
 from .form import PostForm
 
-def index(request): # request는 브라우저의 요청을 의미한다.
-    return HttpResponse('<h1>Hello, Inflearn!</h1>')
-
-def index2(request):
+def index(request):
     lottos = GuessNumbers.objects.all()
     return render(request, "lotto/default.html", {"lottos" : lottos}) # 마지막 괄호에 있는건 딕셔너리 형태로 {}상태로 그냥 놔둬도 무관.
 
