e34279b98cb2b0c54fceb351fb0fa1a3b321b2e2
ldgeao99 <ldgeao99@gmail.com>
2019-02-21 19:59:18 +0900

    function views방식으로 구현되었던 app1, lotto앱을 Including another URLconf 방식으로 변경 해주면서 'lotto'단어가 패턴에서 필요가 없어졌다. 따라서 lotto단어를 제거.

--- mysite/lotto/urls.py
+++ mysite/lotto/urls.py
@@ -15,15 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from lotto import views
+from . import views
+
+app_name = 'lotto'
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls), #localhost:8000/admin일 때
-    url(r'^$', views.index),          #localhost:8000/일 때 views.index에 연결해준다.
-    url(r'^lotto/$', views.index2, name='index'),
-    url(r'^lotto/new/$', views.post, name='new_lotto'),
-    #url(r'^lotto/[0-9]+/detail/$', views.detail1, name='detail1'), #regular expression 사용, localhost:8000/555/detail 같은 링크연결이 가능
-
-    #localhost:8000/555/detail 같은 링크연결이지만 사용자가 링크 클릭시 눌린 객체의 primary key가 url에 자동으로 들어감
-    url(r'^lotto/(?P<lottokey>[0-9]+)/detail/$', views.detail, name='detail'),
+    url(r'^$', views.index, name='index'),
+    url(r'^new/$', views.post, name='new_lotto'),
+    url(r'^(?P<lottokey>[0-9]+)/detail/$', views.detail, name='detail'),
 ]
